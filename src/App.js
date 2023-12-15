bded20ec7bb892d6af5ac97a536bf3073634f693
Flora <laszlo.t.flora@gmail.com>
2023-12-15 19:27:32 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        className="bar"
+        style={{
+          position: "fixed",
+          bottom: "0px",
+          width: "100%",
+          height: "280px",
+          backgroundColor: "hsl(195deg, 20%, 86%)",
+        }}
+      ></div>
       <CharacterEditor />
       <Footer />
     </>
